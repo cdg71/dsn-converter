9f8c45859e46f9da0330b5fe951c98e74be00db8
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2023-05-04 10:48:37 +0200

    feat: processGlobalDsnFiles, splitGlobalDsnFile, createMonthlyDsnFiles

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { promises as fsPromises } from "node:fs";
 
 interface ScriptStatistics {
   start: number;
-  processedFiles: number;
   end?: number;
   duration?: number;
 }
@@ -16,24 +15,27 @@ interface ScriptStatistics {
  */
 (async function main(): Promise<void> {
   console.log("Début de la conversion DSN");
+  // Setup
   const scriptStatistics: ScriptStatistics = {
     start: performance.now(),
-    processedFiles: 0,
   };
-  const limit = 1;
-  const delimiter = /S20.G00.05.001,'01'/;
+  const delimiter = "S20.G00.05.001,'01'\r\n";
+  const payMonthRegex = /S20.G00.05.005,'(\S+)'/;
+  const siretRegex = /S21.G00.06.001,'(\S+)'/;
+  const nafRegex = /S21.G00.06.002,'(\S+)'/;
+  // Conversion
   try {
-    const { inputFolder, outputFolder } = getParameters();
-    const fileNames = await getFiles({ inputFolder });
-    for (const fileName of fileNames) {
-      if (path.extname(fileName) === ".dsn") {
-        const filePath = path.join(inputFolder, fileName);
-        const dsnFileParts = await getDsnFileParts({ filePath, delimiter });
-        //generate the corresponding dsn files
-      }
-      scriptStatistics.processedFiles++;
-      if (scriptStatistics.processedFiles >= limit) break;
-    }
+    const { inputFolderPath, outputFolderPath } = getScriptParameters();
+    const globalDsnFileNames = await getGlobalDsnFiles({ inputFolderPath });
+    await processGlobalDsnFiles({
+      globalDsnFileNames,
+      delimiter,
+      payMonthRegex,
+      siretRegex,
+      nafRegex,
+      inputFolderPath,
+      outputFolderPath,
+    });
   } catch (error) {
     console.error(error);
   } finally {
@@ -48,15 +50,18 @@ interface ScriptStatistics {
 /**
  * Returns an object containing the input file and output folder paths
  *
- * @throws Error if invalid arguments are provided
- * @returns {Object} Object containing the input file and output folder paths
+ * @returns {Object} An object containing the input file and output folder paths
+ * @throws Will throw an error if the file cannot be read
  */
-function getParameters(): { inputFolder: string; outputFolder: string } {
+function getScriptParameters(): {
+  inputFolderPath: string;
+  outputFolderPath: string;
+} {
   console.log("- Lecture des paramètres");
   try {
-    const inputFolder = path.normalize(process.argv[2]);
-    const outputFolder = path.normalize(process.argv[3]);
-    return { inputFolder, outputFolder };
+    const inputFolderPath = path.normalize(process.argv[2]);
+    const outputFolderPath = path.normalize(process.argv[3]);
+    return { inputFolderPath, outputFolderPath };
   } catch (error) {
     throw { message: "Arguments invalides", error };
   }
@@ -64,30 +69,102 @@ function getParameters(): { inputFolder: string; outputFolder: string } {
 
 /**
  * Asynchronously reads the contents of the specified directory and returns an array of file names.
- * @param inputFolder - The path of the folder to read.
- * @returns A promise that resolves with an array of file names.
- * @throws Object containing a message and an error property if the folder cannot be read.
+ * @async
+ * @param {Object} options - The options object
+ * @param {string} options.inputFolderPath - The input folder path
+ * @returns {Promise<string[]>} An array of file names
+ * @throws Will throw an error if the file cannot be read.
  */
-async function getFiles({
-  inputFolder,
+async function getGlobalDsnFiles({
+  inputFolderPath,
 }: {
-  inputFolder: string;
+  inputFolderPath: string;
 }): Promise<string[]> {
   console.log("- Listing des fichiers");
   try {
-    const files = await fsPromises.readdir(inputFolder);
+    const files = await fsPromises.readdir(inputFolderPath);
     return files;
   } catch (error) {
     throw { message: "Impossible de lister les fichiers", error };
   }
 }
 
-async function getDsnFileParts({
+/**
+ * Processes the global DSN files
+ * @async
+ * @param {Object} options - The options object
+ * @param {string[]} options.globalDsnFileNames - An array of global DSN file names
+ * @param {string} options.delimiter - Delimiter used in the DSN files
+ * @param {string} options.payMonthRegex - Regex used to extract the PAY_MONTH
+ * @param {string} options.siretRegex - Regex used to extract the SIREN
+ * @param {string} options.nafRegex - Regex used to extract the NAF
+ * @param {string} options.inputFolderPath - The input folder path
+ * @param {string} options.outputFolderPath - The output folder path
+ * @throws Throws an error if the file cannot be written
+ * @returns {Promise<void>} A promise that resolves when the file is written
+ */
+async function processGlobalDsnFiles({
+  globalDsnFileNames,
+  delimiter,
+  payMonthRegex,
+  siretRegex,
+  nafRegex,
+  inputFolderPath,
+  outputFolderPath,
+}: {
+  globalDsnFileNames: string[];
+  delimiter: string;
+  payMonthRegex: RegExp;
+  siretRegex: RegExp;
+  nafRegex: RegExp;
+  inputFolderPath: string;
+  outputFolderPath: string;
+}): Promise<void> {
+  try {
+    const limit = 1;
+    let processedFiles = 0;
+    for (const fileName of globalDsnFileNames) {
+      if (path.extname(fileName) === ".dsn") {
+        const filePath = path.join(inputFolderPath, fileName);
+        const globalDsnFileParts = await splitGlobalDsnFile({
+          filePath,
+          delimiter,
+        });
+        await createMonthlyDsnFiles({
+          globalDsnFileParts,
+          delimiter,
+          payMonthRegex,
+          siretRegex,
+          nafRegex,
+          outputFolderPath,
+        });
+      }
+      processedFiles++;
+      if (processedFiles >= limit) break;
+    }
+  } catch (error) {
+    throw {
+      message: "Impossible de traiter les fichiers DSN globaux",
+      error,
+    };
+  }
+}
+
+/**
+ * splits the contents of a global DSN file into parts, given its file path and a delimiter.
+ * @async
+ * @param {object} options - The options object.
+ * @param {string} options.filePath - The path of the file to read.
+ * @param {string} options.delimiter - The delimiter to split the file contents on.
+ * @returns {Promise<string[]>} - The parts of the DSN file.
+ * @throws Will throw an error if the file cannot be read.
+ */
+async function splitGlobalDsnFile({
   filePath,
   delimiter,
 }: {
   filePath: string;
-  delimiter: RegExp;
+  delimiter: string;
 }): Promise<string[]> {
   console.log(`- Lecture du fichier '${filePath}'`);
   try {
@@ -96,8 +173,103 @@ async function getDsnFileParts({
     return fileParts;
   } catch (error) {
     throw {
-      message: `Impossible de lire le fichier '${filePath}'`,
+      message: `Impossible de découper le fichier '${filePath}'`,
       error,
     };
+  }
+}
+
+/**
+ * Creates monthly DSN files in the output folder from one global DSN file
+ *
+ * @param {Object} options - The options object
+ * @param {string[]} options.dsnFileParts - An array of DSN file parts
+ * @param {string} options.delimiter - Delimiter used in the DSN files
+ * @param {string} options.payMonthRegex - Regex used to extract the PAY_MONTH
+ * @param {string} options.siretRegex - Regex used to extract the SIREN
+ * @param {string} options.nafRegex - Regex used to extract the NAF
+ * @param {string} options.outputFolder - The output folder path
+ * @throws Throws an error if the file cannot be written
+ * @returns {Promise<void>} A promise that resolves when the file is written
+ */ async function createMonthlyDsnFiles({
+  globalDsnFileParts,
+  delimiter,
+  payMonthRegex,
+  siretRegex,
+  nafRegex,
+  outputFolderPath,
+}: {
+  delimiter: string;
+  globalDsnFileParts: string[];
+  payMonthRegex: RegExp;
+  siretRegex: RegExp;
+  nafRegex: RegExp;
+  outputFolderPath: string;
+}): Promise<void> {
+  console.log("  - Création des fichiers DSN mensuels");
+  try {
+    const header = globalDsnFileParts[0];
+    const parts = globalDsnFileParts.slice(1);
+    for (const part of parts) {
+      const payMonth = extractFirstCapturingGroup({
+        regex: payMonthRegex,
+        text: part,
+      });
+      const year = payMonth.slice(4);
+      const month = payMonth.slice(2, 4);
+      const day = payMonth.slice(0, 2);
+      const siret = extractFirstCapturingGroup({
+        regex: siretRegex,
+        text: part,
+      });
+      const naf = extractFirstCapturingGroup({
+        regex: nafRegex,
+        text: part,
+      });
+      const outputfileName = `${siret}${naf}_${year}-${month}-${day}.dsn`;
+      const outputFilePath = path.join(outputFolderPath, outputfileName);
+      const fileContents = [header, delimiter, part].join("");
+      await createFolderIfNotExists({ outputFolderPath });
+      await fsPromises.writeFile(outputFilePath, fileContents, "latin1");
+    }
+  } catch (error) {
+    throw { message: `Impossible de créer un fichier DSN mensuel`, error };
+  }
+}
+
+/**
+ * Extracts the first capturing group from a given string using the provided regular expression.
+ * @param regex - The regular expression to use for matching.
+ * @param text - The string to extract the capturing group from.
+ * @returns The first capturing group if found, otherwise an empty string.
+ */
+function extractFirstCapturingGroup({
+  regex,
+  text,
+}: {
+  regex: RegExp;
+  text: string;
+}): string {
+  const match = regex.exec(text);
+  return match?.[1] ?? "";
+}
+
+/**
+ * Creates a folder if it does not exist
+ * @async
+ * @param {Object} options - The options object
+ * @param {string} options.outputFolderPath - The output folder path
+ * @throws Throws an error if the folder cannot be created
+ * @returns {Promise<void>} A promise that resolves when the folder is created
+ */
+async function createFolderIfNotExists({
+  outputFolderPath,
+}: {
+  outputFolderPath: string;
+}): Promise<void> {
+  try {
+    await fsPromises.access(outputFolderPath, fsPromises.constants.F_OK);
+  } catch (error) {
+    await fsPromises.mkdir(outputFolderPath, { recursive: true });
   }
 }
